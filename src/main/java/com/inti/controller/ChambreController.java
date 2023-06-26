45610218870aa0d75f505173869c50edd034a731
Daenrya <marjolaine.arb@gmail.com>
2023-06-26 15:15:17 +0200

    liste chambre + modifier chambre

--- src/main/java/com/inti/controller/ChambreController.java
+++ src/main/java/com/inti/controller/ChambreController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -36,6 +37,27 @@ public class ChambreController {
 		return "listeChambre";
 	}
 	
+	@GetMapping("deleteChambre/{id}")
+	public String deleteChambre(@PathVariable("id") int id) {
+		ichr.deleteById(id);
+
+		return "redirect:/chambre/listeChambre";
+		
+	}
+	
+	@GetMapping("modifierChambre/{id}")
+	public String modifierChambre(@PathVariable("id") int id, Model m) {
+		m.addAttribute("chambre", ichr.getById(id));
+
+		return "modifierChambre";
+	}
+	
+	@PostMapping("modifierChambre/updateChambre")
+	public String updateChambre(@ModelAttribute("chambre") Chambre c)
+	{
+		ichr.save(c);
+		return "redirect:/chambre/listeChambre";
+	}
 	
 
 	
