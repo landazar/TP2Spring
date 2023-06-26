16ddc09261f49b103f05a3c98c80bbce7ab7da76
Emdgn <degeneve@kth.se>
2023-06-26 16:09:28 +0200

    Securite

--- src/main/java/com/inti/controller/MainController.java
+++ src/main/java/com/inti/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.inti.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.inti.model.Role;
 import com.inti.model.Salarie;
@@ -34,8 +36,10 @@ public class MainController {
 		return "creerSalarieConnexion";
 	}
 	@PostMapping("creerSalarieConnexion")
-	public String creerSalarie(@ModelAttribute("salarie") Salarie salarie) {
+	public String creerSalarie(@RequestParam("type") String type, @RequestParam("dateNaissance") String dateNaissance,
+			@RequestParam("dateEmbauche") String dateEmbauche, @RequestParam("login") String login, @RequestParam("mdp") String mdp) {
 		
+		Salarie salarie = new Salarie(login, mdp, type, LocalDate.parse(dateNaissance), LocalDate.parse(dateEmbauche));
 		BCryptPasswordEncoder b = new BCryptPasswordEncoder();
 		String mdpCrypte = b.encode(salarie.getMdp());
 		salarie.setMdp(mdpCrypte);
